884cd738ea4af13860a48f39bd14dc129d011c91
abel <abelorode@gmail.com>
2022-03-05 10:11:41 +0000

    node-sass package initialized and configured

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 const App = () => {
   return (
     <>
-      <h1>This is a functional component</h1>
+      <div className="container">
+        <div className="row p-15 mt-15">
+          <div className="col-6" id="one">
+            <h1>This is a functional component in a column 6 with id one</h1>
+          </div>
+          <div className="col-6" id="two">
+            <h1>This is a functional component in a column 6 with id two</h1>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
